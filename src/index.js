ba0a3ebe3a8ddcbeacc4e12a421b51e90acdca64
akin.ipek0505@gmail.com <akin.ipek0505@gmail.com>
2024-02-08 14:32:25 +0000

    added styles and assets folders, added ExternalLink and Loading components

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import VSpinner from "@/components/VSpinner"
+import ExternalLink from "./components/base/ExternalLink";
+import Loading from "./components/base/Loading";
 
-export { VSpinner }
+export { VSpinner, ExternalLink, Loading }
 
 // plugin to register globally for app.use(optional depending on how we want to use the components)
 export default {
     install: (app, options) => {
         // set the name of the component
-        app.component("VSpinner", VSpinner);
+        // app.component("VSpinner", VSpinner);
     }
 }
