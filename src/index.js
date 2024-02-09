cecc0fe6594228769e0bc554028d1d78a8cce323
akin.ipek0505@gmail.com <akin.ipek0505@gmail.com>
2024-02-09 13:06:06 +0000

    tooltip component added

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import VSpinner from "@/components/base/VSpinner"
 import ExternalLink from "./components/base/ExternalLink";
 import Loading from "./components/base/Loading";
+import Tooltip from "./components/base/Tooltip";
 
-export { VSpinner, ExternalLink, Loading }
+export { VSpinner, ExternalLink, Loading, Tooltip }
 
 // plugin to register globally for app.use(optional depending on how we want to use the components)
 export default {
