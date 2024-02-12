16cff896eb6132272d86b75ffa820fcd7ef60d03
akin.ipek0505@gmail.com <akin.ipek0505@gmail.com>
2024-02-12 11:48:11 +0000

    exported modal component

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import VSpinner from "@/components/base/VSpinner"
-import ExternalLink from "./components/base/ExternalLink";
-import Loading from "./components/base/Loading";
-import Tooltip from "./components/base/Tooltip";
+import ExternalLink from "@/components/base/ExternalLink";
+import Loading from "@/components/base/Loading";
+import Tooltip from "@/components/base/Tooltip";
+import Modal from "@/components/base/Modal"
 
-export { VSpinner, ExternalLink, Loading, Tooltip }
+export { VSpinner, ExternalLink, Loading, Tooltip, Modal }
 
 // plugin to register globally for app.use(optional depending on how we want to use the components)
 export default {
