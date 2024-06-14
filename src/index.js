24fdd695aad57db0cdd18a5ad81f09979b9009e2
akin.ipek0505@gmail.com <akin.ipek0505@gmail.com>
2024-06-14 20:00:11 +0300

    updated shadcn select component

--- src/index.js
+++ src/index.js
@@ -32,3 +32,11 @@ export {
   QuickActionsBtn,
   ShadSelect
 }
+
+// plugin to register globally for app.use(optional depending on how we want to use the components)
+export default {
+  install: (app, options) => {
+    // set the name of the component
+    app.component("VSpinner", VSpinner);
+  },
+}
