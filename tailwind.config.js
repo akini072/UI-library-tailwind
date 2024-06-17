8dfc43009557cfab675d3fb29130a24e06d32d27
akin.ipek0505@gmail.com <akin.ipek0505@gmail.com>
2024-06-17 09:44:17 +0300

    update tailwindcss config

--- tailwind.config.js
+++ tailwind.config.js
@@ -62,10 +62,10 @@ module.exports = {
         },
       },
       borderRadius: {
-        xl: 'calc(var(--radius) + 4px)',
-        lg: 'var(--radius)',
-        md: 'calc(var(--radius) - 2px)',
-        sm: 'calc(var(--radius) - 4px)',
+        xl: '1.5rem',
+        lg: '1rem',
+        md: '0.375rem',
+        sm: '0.125rem',
       },
       keyframes: {
         'accordion-down': {
