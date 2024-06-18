36a94ae71555832ded15ff35b633cb5a6f24b2b2
akin.ipek0505@gmail.com <akin.ipek0505@gmail.com>
2024-06-18 17:27:07 +0300

    fix-issue-2-added hamburger svg icon and chanced input border color

--- src/components/shadcn/input/Input.vue
+++ src/components/shadcn/input/Input.vue
@@ -25,7 +25,7 @@ const modelValue = useVModel(props, "modelValue", emits, {
     :width="width"
     :class="
       cn(
-        'flex h-10 w-full rounded-md border-[1px] border-solid border-[var(--border)] bg-white px-3 py-2 text-sm ring-offset-white file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-[var(--gray-500)] disabled:cursor-not-allowed disabled:opacity-50 dark:border-slate-800 dark:bg-slate-950 dark:ring-offset-slate-950 dark:placeholder:text-slate-400 dark:focus-visible:ring-slate-300',
+        'flex h-10 w-full rounded-md border-[1px] border-solid border-[var(--gray-200)] bg-white px-3 py-2 text-sm ring-offset-white file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-[var(--gray-500)] disabled:cursor-not-allowed disabled:opacity-50 dark:border-slate-800 dark:bg-slate-950 dark:ring-offset-slate-950 dark:placeholder:text-slate-400 dark:focus-visible:ring-slate-300',
         props.class,        
       )
     "
