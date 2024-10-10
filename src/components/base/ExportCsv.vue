50840c9cb61fe30b0a4a6c7ae05c4c006daa5dc4
akin.ipek0505@gmail.com <akin.ipek0505@gmail.com>
2024-10-10 06:40:04 +0200

    Install neccessary

---
+++ src/components/base/ExportCsv.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-popup width="560px" :title="title" @close="$emit('cancel')">
+    <h5>Export</h5>
+  </v-popup>
+</template>
+
+<script>
+import { defineAsyncComponent } from 'vue';
+import { saveAs } from 'file-saver'
+import moment from 'moment'
+export default {
+  name: 'ExportCsv',
+  components: {
+    VButton: defineAsyncComponent(() => import('@/components/base/VButton')),
+    RangeDatePicker: defineAsyncComponent(() => import('@/components/base/RangeDatePicker')),
+    VIcon: defineAsyncComponent(() => import('@/components/base/VIcon')),
+    VPopup: defineAsyncComponent(() => import('@/components/base/VPopup')),
+  },
+  props: {
+    title: { type: String },
+    loadingExport: { type: Boolean },
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
