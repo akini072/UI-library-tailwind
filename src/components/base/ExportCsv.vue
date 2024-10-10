87b546133139c7ba160a0606fbfdd0a28e8a0844
akin.ipek0505@gmail.com <akin.ipek0505@gmail.com>
2024-10-10 06:59:49 +0200

    Frame

--- src/components/base/ExportCsv.vue
+++ src/components/base/ExportCsv.vue
@@ -1,6 +1,12 @@
 <template>
-  <v-popup width="560px" :title="title" @close="$emit('cancel')">
-    <h5>Export</h5>
+  <v-popup width="560px" :title="title" @close="$emit('close')">
+    <template #body>
+      <div class="automation-progress-panel popup_progress_panel ma-4">
+        <div class="item-prog d-flex" :class="{ deactivated: step.status === 'deactivated' }" v-for="step in steps"
+          :key="step.id">
+        </div>
+      </div>
+    </template>
   </v-popup>
 </template>
 
@@ -19,9 +25,61 @@ export default {
   props: {
     title: { type: String },
     loadingExport: { type: Boolean },
+  },
+  data() {
+    const startDate = moment().startOf('day').toDate()
+    const endDate = moment().endOf('day').toDate()
+    const date = {
+      startDate,
+      endDate,
+      currentRange: 'Last 30 days'
+    }
+    return {
+      date: date,
+      exporting: false,
+      exportData: {
+        id: 0,
+        funnel_id: 0
+      },
+      steps: [
+        {
+          id: 1,
+          name: 'Select orders to export',
+          step: 'Step1',
+          status: 'active'
+        },
+        {
+          id: 2,
+          name: 'Exporting your orders...',
+          step: 'Step2',
+          status: 'deactivated'
+        },
+        {
+          id: 3,
+          name: 'Exporting completed',
+          step: 'Step3',
+          status: 'deactivated'
+        }
+      ],
+      stepState: 1,
+      ordersBy: 'all',
+      progressNumber: 0
+    }
   }
 }
 </script>
 
 <style lang="scss" scoped>
+.automation-progress-panel {
+  display: flex;
+  justify-content: space-between;
+  &.popup_progress_panel {
+    flex-wrap: wrap;
+    padding: 2px;
+    margin-bottom: 20px;
+  }
+  .item-prog {
+    margin: 5px;
+  }
+}
 </style>
