400c014fe95fe3b9368c143bb5f0b0603ed9065d
akin.ipek0505@gmail.com <akin.ipek0505@gmail.com>
2025-01-22 14:52:12 -0300

    remove console

--- src/components/images/SelectImagePopup.vue
+++ src/components/images/SelectImagePopup.vue
@@ -277,7 +277,6 @@ export default {
     },
 
     getPexelsImages(limit) {
-      console.log('pexels: ', import.meta.env.VITE_PEXELS_KEY)
       fetch(`https://api.pexels.com/v1/curated?per_page=${limit}`, {
         headers: {
           Authorization: import.meta.env.VITE_PEXELS_KEY,
