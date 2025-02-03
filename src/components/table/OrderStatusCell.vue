0f65c27a0e4cc95817ee925dbec76e753e992c90
akin.ipek0505@gmail.com <akin.ipek0505@gmail.com>
2025-02-03 22:29:16 +0200

    updated the OrderStatusCell

--- src/components/table/OrderStatusCell.vue
+++ src/components/table/OrderStatusCell.vue
@@ -1,11 +1,11 @@
 <template>
     <span
-      class="status d-flex align-center"
+      class="status rounded-lg font-weight-medium d-flex align-center"
       :class="{
-        failed: value === 'Failed' || value === 'Cancelled',
-        archived: value === 'Draft' || value === 'Archieved',
-        success: value === 'Succeeded' || value === 'Fulfilled',
-        processing: value === 'Processing' || value === 'Refunded',
+        'border--error-200 bg-error-50 text--error-700': value === 'Failed' || value === 'Cancelled',
+        'border--gray-200 bg--gray-50 text-gray-00': value === 'Draft' || value === 'Archieved',
+        'border--success-200 bg--success-50 text--success-700': value === 'Succeeded' || value === 'Fulfilled',
+        'border--warning-200 bg--warning-50 text--warning-700': value === 'Processing' || value === 'Refunded',
       }"
       :style="row.is_archived ? { opacity: 0.5 } : {}"
     >
@@ -23,37 +23,11 @@ export default {
       type: Object,
     },
   },
-};
+}
 </script>
 <style lang="scss" scoped>
 .status {
-  font-weight: 500;
   padding: 1px 8px;
-  border-radius: 16px;
   display: inline-block;
-
-  &.success {
-    border: 1px solid #abefc6;
-    background-color: #ecfdf3;
-    color: #067647;
-  }
-
-  &.processing {
-    border: 1px solid #fedf89;
-    background-color: #fffaeb;
-    color: #b54708;
-  }
-
-  &.failed {
-    border: 1px solid #fecdca;
-    background-color: #fef3f2;
-    color: #b42318;
-  }
-
-  &.archived {
-    border: 1px solid #e4e6f1;
-    background-color: #f9fafb;
-    color: #344054;
-  }
 }
 </style>
