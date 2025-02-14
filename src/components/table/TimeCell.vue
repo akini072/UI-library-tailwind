d4bdf7ba044b0d33a5c2f1b4e60e766c60fab74b
akin.ipek0505@gmail.com <akin.ipek0505@gmail.com>
2025-02-14 06:25:55 +0100

    Remove obsolete prop

--- src/components/table/TimeCell.vue
+++ src/components/table/TimeCell.vue
@@ -16,14 +16,13 @@ export default {
       type: String,
       default: 'Europe/London'
     },
-    dateField: {
-      // <key of the date-time field that should be displayed>
-      type: String,
+    value: {
+      type: String
     }
   },
   computed: {
     formattedDate() {
-      const d = this.dateField ? new Date(this.row[this.dateField]).toISOString() : new Date(this.row.updated_at).toISOString()
+      const d = new Date(this.value).toISOString()
       let x = moment.tz(d, this.timeZone)
       let diff = (new Date() - x) / 3600 / 1000
       return diff > 24 ? x.format('MMM D, LT') : moment(x).fromNow()
